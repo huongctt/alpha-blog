2fc9f93ac5ebb3a6d69a6a180d34e9dee28c4fe9
huongctt <thanhhuong12333@gmail.com>
2021-10-12 14:51:08 +0700

    add profile picture and listing article

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
   resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
-  resources :users, only: [:create, :edit, :update]
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
