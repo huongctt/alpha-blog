e1331aeecd9792fd8e1d229bd10594b5726c42ad
huongctt <thanhhuong12333@gmail.com>
2021-10-12 09:22:53 +0700

    add hash password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     has_many :articles
     validates :username, presence: true, 
                         uniqueness: true, 
-                        length: {minimum:1, maximum:5}
+                        length: {minimum:1, maximum:25}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, 
                         length: { maximum:105},
                         format: {with: VALID_EMAIL_REGEX}
+    has_secure_password
 end
