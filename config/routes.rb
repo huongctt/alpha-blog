50c2f31e53c31b9fd797d5ecc84c8a8efd0637d3
huongctt <thanhhuong12333@gmail.com>
2021-10-12 10:30:54 +0700

    Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
   resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :users, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
