e4af66420b609e8a8fe2bf3ed96d00343c15eba8
huongctt <thanhhuong12333@gmail.com>
2021-10-13 15:05:52 +0700

    add session and restrict user action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
   resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
-  resources :users
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
